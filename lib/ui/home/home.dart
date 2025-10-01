d64aa26a409e41133f7144cbd48daac67dc28842
trung1609 <vmt162005@gmail.com>
2025-10-01 15:35:21 +0700

    Thiet kế giao diện homepage và hiển thị danh sách bài hát

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_app/data/model/song.dart';
 import 'package:mobile_app/ui/discovery/discovery.dart';
+import 'package:mobile_app/ui/home/viewmodel.dart';
 import 'package:mobile_app/ui/setting/setting.dart';
 import 'package:mobile_app/ui/user/user.dart';
 
@@ -44,11 +46,20 @@ class _MusicHomePageState extends State<MusicHomePage> {
         tabBar: CupertinoTabBar(
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.album), label: "Discovery",),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.album),
+              label: "Discovery",
+            ),
             BottomNavigationBarItem(icon: Icon(Icons.person), label: "Account"),
-            BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Setting",),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Setting",
+            ),
           ],
-          backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .onInverseSurface,
         ),
         tabBuilder: (BuildContext context, int index) {
           return _tabs[index];
@@ -63,6 +74,111 @@ class HomeTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Home App")));
+    return HomeTabPage();
+  }
+}
+
+class HomeTabPage extends StatefulWidget {
+  const HomeTabPage({super.key});
+
+  @override
+  State<HomeTabPage> createState() => _HomeTabPageState();
+}
+
+class _HomeTabPageState extends State<HomeTabPage> {
+  List<Song> songs = [];
+  late MusicAppViewModel _viewModel;
+
+  @override
+  void initState() {
+    _viewModel = MusicAppViewModel();
+    _viewModel.loadSong();
+    observeData();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _viewModel.songStream.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: getBody());
+  }
+
+  Widget getBody() {
+    bool showLoading = songs.isEmpty;
+    if (showLoading) {
+      return getProgressBar();
+    } else {
+      return getListView();
+    }
+  }
+
+  Widget getProgressBar() {
+    return const Center(child: CircularProgressIndicator());
+  }
+
+  Widget getListView() {
+    return ListView.separated(
+      itemBuilder: (context, position) {
+        return getRow(position);
+      },
+      separatorBuilder: (context, index) {
+        return const Divider(
+          color: Colors.grey,
+          thickness: 1,
+          indent: 24,
+          endIndent: 24,
+        );
+      },
+
+      itemCount: songs.length,
+      shrinkWrap: true,
+    );
+  }
+
+  Widget getRow(int index) {
+    return _SongItemSection(parent: this, song: songs[index]);
+  }
+
+  void observeData() {
+    _viewModel.songStream.stream.listen((songList) {
+      setState(() {
+        songs.addAll(songList);
+      });
+    });
+  }
+}
+
+class _SongItemSection extends StatelessWidget {
+  const _SongItemSection({required this.parent, required this.song});
+
+  final _HomeTabPageState parent;
+  final Song song;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: const EdgeInsets.only(left: 24, right: 8),
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: FadeInImage.assetNetwork(
+          placeholder: 'assets/ITunes_12.2_logo.png',
+          image: song.image,
+          width: 48,
+          height: 48,
+          imageErrorBuilder: (context, error, stackTrace) {
+            return Image.asset(
+              'assets/ITunes_12.2_logo.png', width: 48, height: 48,);
+          },
+        ),
+      ),
+      title: Text(song.title),
+      subtitle: Text(song.artist),
+      trailing: IconButton(onPressed: () {}, icon: const Icon(Icons.more_horiz)),
+    );
   }
 }
---
+++ lib/ui/home/viewmodel.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+import 'package:mobile_app/data/repository/repository.dart';
+
+import '../../data/model/song.dart';
+
+class MusicAppViewModel {
+  StreamController<List<Song>> songStream = StreamController();
+
+  void loadSong() {
+    final repository = DefaultRepository();
+    repository.loadData().then((value) => songStream.add(value!));
+  }
+}
